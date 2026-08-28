59a7ff5d63bfc441fab6bdcfbb73efba9bfc836e
k sri satya raghavendra <raghukodavati2@gmail.com>
2026-08-29 02:20:38 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-from bs4 import beautifulsoup4
+from bs4 import BeautifulSoup
+
+import requests
+search_term = input("Enter the product you want to search for: ")
+html_text = requests.get(f'https://mdcomputers.in/?route=product/search&search={search_term}').text
+print(html_text);
